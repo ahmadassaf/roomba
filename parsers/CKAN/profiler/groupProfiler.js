97669e3c5ce4fa6b3c1b7aed35aec4ae766a9f76
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2015-01-12 18:36:40 +0100

    adding the profile aggregation function calls

--- parsers/CKAN/profiler/groupProfiler.js
+++ parsers/CKAN/profiler/groupProfiler.js
@@ -6,8 +6,10 @@ function groupProfiler(parent) {
 
 	extend(this, parent);
 
-	var groupProfiler = this;
-	var _             = this.util._;
+	var groupProfiler   = this;
+	var _               = this.util._;
+
+	var aggregateReport = new profile(this);
 
 	this.profileGroup = function profileGroup(profilerCallback) {
 
@@ -23,7 +25,8 @@ function groupProfiler(parent) {
 			groupProfiler.crawler.getGroupDetails(function(error, data, message, groupList){
 				if (!error)
 					groupProfiler.generateGroupProfiles(groupList.result.packages, saveProfile, cachedProfiles, function(error, aggregateReport){
-						console.log(aggregateReport);
+						console.log(aggregateReport.getProfile());
+						aggregateReport.prettyPrint();
 						/* To Do : The saving process and prompt */
 						!error ? profilerCallback(false, false, {type: "info", message: "profilingCompleted"}) : profilerCallback(false, false, {type: "error", message: "profilingFailed"});
 					});
@@ -36,7 +39,7 @@ function groupProfiler(parent) {
 
 		var folderName      = this.datasetsFolder;
 		var pace            = require('awesome-progress')({total: groupList.length, finishMessage: this.options.info.datasetsFetched, errorMessage: this.options.error.parseError});
-		var aggregateReport = {};
+		var profilingErrors = [];
 
 		// Parse through the dataset list items and fetch the corresponding JSON file
 		groupProfiler.async.eachLimit(groupList,0.0001,function(item, asyncCallback){
@@ -49,6 +52,7 @@ function groupProfiler(parent) {
 				groupProfiler.cache.getCache(groupProfiler.profilesFolder + "/" + item.name, function(error, file){
 					if (!error) {
 						// cache file has been found successfully, do the needed statistics and aggregations and go to next dataset
+						aggregateReport.mergeReports(aggregateReport.getProfile(), file);
 						tick();
 					} else retreiveProfiles(fileName, url);
 				});
@@ -56,13 +60,14 @@ function groupProfiler(parent) {
 
 			function retreiveProfiles(fileName, url) {
 
-				var report = new profile(this);
+				var report          = new profile(this);
 
 				groupProfiler.util.download(groupProfiler.cache, fileName, url, function(error, dataset){
 					// If the file has been fetched successfully log it into the error.json
 					if (error) tick({errors: 1});
 						// Checks if the file has been already cached
 						else {
+							// Start the profiing tasks assigned
 							groupProfiler.metadataProfiler.generalProfiler.start(dataset, function(error, generalReport){
 								if (!error) groupProfiler.metadataProfiler.ownershipProfiler.start(dataset, function(error, ownershipReport){
 									if (!error) groupProfiler.metadataProfiler.provenanceProfiler.start(dataset, function(error, provenanceReport){
@@ -71,6 +76,8 @@ function groupProfiler(parent) {
 
 												// merge the various metadata reports
 												report.mergeReportsUniquely([generalReport, ownershipReport, provenanceReport, accessReport]);
+												aggregateReport.mergeReports(aggregateReport.getProfile(), report.getProfile());
+
 												// check if the user has selected he wishes to save the profile and enhanced profile
 												if (saveProfile) {
 													groupProfiler.cache.setCache(groupProfiler.profilesFolder + item.name, report.getProfile(), function(error){
@@ -78,16 +85,16 @@ function groupProfiler(parent) {
 															// check if there is an enhanced profile and the user wishes to save it
 															if (profileChanged) {
 																groupProfiler.cache.setCache(groupProfiler.enrichedFolder + item.name, enhancedProfile, function(error){
-																	if (!error) {
-																		//aggregateReport[item.name] = groupProfiler.util.mergeObjects({}, [generalReport, ownershipReport, provenanceReport, accessReport]);
-																		tick();
-																	} else tick();
+																	if (!error) tick();
 																});
 															} else tick();
-														} else tick();
+														}
 													});
 												} else tick();
-											} else tick();
+											} else {
+												tick({errors: 1});
+												profilingErrors.push(dataset.name);
+											}
 										});
 									});
 								});
@@ -102,6 +109,8 @@ function groupProfiler(parent) {
 				asyncCallback();
 			}
 		},function(err){
+			if (profilingErrors.length > 0 )
+				console.log("We couldn't profile the following Datasets: " + profilingErrors);
 			// The dataset lists has been successfully parsed, check if there has been any errors and save them to errors.json
 			!_.isEmpty(aggregateReport) ? callback(false, aggregateReport) : callback(true,aggregateReport);
 		});
