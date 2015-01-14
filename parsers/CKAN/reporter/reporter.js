c61ccbf17b77528081fc73be6d325117ffa33ce8
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2015-01-14 16:40:08 +0100

    adding reporter.js comments

--- parsers/CKAN/reporter/reporter.js
+++ parsers/CKAN/reporter/reporter.js
@@ -14,10 +14,24 @@ function profiler(parent) {
 
 };
 
+/**
+* Generate a portal-wide report by aggregating all the dataset reports in the portal
+*
+* @method generatePortalReport
+* @param {Function} callback: When successfull returns a false error with a success message -> callback(false, false, SuccessMessage)
+*                             When failed returns a true error with a fail message -> callback(true, false, ErrorMessage)
+*/
 profiler.prototype.generatePortalReport = function generatePortalReport(parserInterfaceCallback) {
 	this.reportGenerator.generatePortalReport(parserInterfaceCallback);
 }
 
+/**
+* Generate a group-wide report by aggregating all the dataset reports in that group
+*
+* @method generateGroupReport
+* @param {Function} callback: When successfull returns a false error with a success message -> callback(false, false, SuccessMessage)
+*                             When failed returns a true error with a fail message -> callback(true, false, ErrorMessage)
+*/
 profiler.prototype.generateGroupReport = function generateGroupReport(parserInterfaceCallback) {
 	this.reportGenerator.generateGroupReport(parserInterfaceCallback);
 }
