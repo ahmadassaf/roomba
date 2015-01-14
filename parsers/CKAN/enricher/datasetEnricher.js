3e9e74ea9d0f47693fd610d78a943676b20d9fa5
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2015-01-14 16:19:56 +0100

    removing unneeded code from enricher

--- parsers/CKAN/enricher/datasetEnricher.js
+++ parsers/CKAN/enricher/datasetEnricher.js
@@ -12,13 +12,3 @@ function datasetEnricher(parent) {
 
 module.exports = datasetEnricher;
 
-
-// On Enricing success callback is: profilerCallback(false, false, {type: "info", message: "enriching"});
-/*
-	-> Checking for valid .csv files structure
-	var parse = require('csv-parse');
-	parse(body, {comment: '#'}, function(err, output){
-	  console.log(output);
-	});
- */
-
